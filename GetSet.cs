d9f44170606a41c0a67268510ea115620d2db334
Ahlam Fazlu Rahman <102663986+Ahlam-Banu@users.noreply.github.com>
2024-02-24 23:58:38 +0200

    getters and setters in C#

---
+++ GetSet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string studentNumber { get; set; }
+        public string email { get; set; }
+        public int phone { get; set; }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Student student = new Student();
+
+            student.studentNumber = "Ahlam";
+            student.email = "ahlam@gmail.com";
+            student.phone = 123456789;
+
+            Console.WriteLine($"Student Number: {student.studentNumber}, Email: {student.email}, Phone: {student.phone}");
+
+        }
+    }
+}
